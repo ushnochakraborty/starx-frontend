fd8a4c22b11f076f2df98cf77ff0562f85e6d787
Andrew Mahn <andrewmahn@Andrews-M1-MBP.attlocal.net>
2022-11-01 23:36:53 -0400

    Rough landing page

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import logo from './assets/images/logo.PNG';
+import './App.css';
+import { CssBaseline, Grid, Typography, Card, CardContent } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import NavBar from './components/NavBar';
+
+const theme = createTheme({
+  palette: {
+    background: {
+      primary: '#FFF',
+      secondary: '#62C6F2',
+    },
+    text: {
+      primary: '#000',
+      secondary: '#62C6F2',
+    }
+  }
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <NavBar />
+      <Grid container spacing={2}
+        direction="row"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: '90vh' }}
+      >
+        <Grid container xs={12} rowSpacing={3} alignItems="center" justifyContent="center" height="50vh">
+          <Grid item xs={12}>
+            <Typography variant="h3" align="center" padding="10px">Welcome to the STARx Survey Tool</Typography>
+            <Typography variant="h3" align="center" padding="10px">Bienvenida a la STARx Encuesta</Typography>
+          </Grid>
+        </Grid>
+        <Grid item xs={6} align="center">
+          <Card sx={{ minWidth: "75%", maxWidth: "75%", bgcolor: "background.secondary", boxShadow: 10}}>
+            <CardContent>
+              <Typography variant="h5" align="center" paddingBottom="10px">English</Typography>
+              <Typography a>Create a new account</Typography>
+              <Typography a>Log into an existing account</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={6} align="center">
+          <Card sx={{ minWidth: "75%", maxWidth: "75%", bgcolor: "background.secondary", boxShadow: 10}}>
+            <CardContent>
+              <Typography variant="h5" align="center" paddingBottom="10px">Español</Typography>
+              <Typography a>Crea una cuenta nueva</Typography>
+              <Typography a>Iniciar sesión en una cuenta existente</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}
+
+export default App;
