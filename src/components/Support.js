50b2b8f1400793be5fed5d54a4b19252278dfa9f
Zhenkai Zhu <zkzhu@email.unc.edu>
2023-04-26 15:46:08 -0400

    update the support page

--- src/components/Support.js
+++ src/components/Support.js
@@ -5,12 +5,12 @@ export default function Support() {
         <h1 style={{ color: "#7BAFD4" }}>Support</h1>
         <p>
           For help understanding how to take the survey and interpret survey
-          results please refer to the STARx Program Home Page
+          results please refer to the <a href="https://www.med.unc.edu/transition/about-us-1/">STARx Program Home Page</a>
         </p>
         <br></br>
         <p>
           For technical support or support using the application please reach
-          out to our developer team at apaulsen43@gmail.com
+          out to our developer team at xhn20001106@gmail.com
         </p>
       </center>
     </div>
